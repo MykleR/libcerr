728894718fec6ada9643ce9c78db1c720c15a1ac
MykleR <michael.rouves@gmail.com>
2025-09-11 19:03:40 +0200

    🚧wip: Jump based try catch, more unit tests

--- headers/libcerr-exception.h
+++ headers/libcerr-exception.h
@@ -4,56 +4,75 @@
 # include <stdlib.h>
 # include <setjmp.h>
 
-# if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L
-#  define ERR_TLS _Thread_local
-# else
-  // For older compilers (GCC/Clang) this typically works:
+#if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L
+  #define ERR_TLS _Thread_local
+#else
   #define ERR_TLS __thread
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// ╔═══════════════════════════════[ DEFINITION ]══════════════════════════════╗
+
+# ifndef ERR_MSG_SIZE
+#  define ERR_MSG_SIZE 1024
 # endif
-
-// ╔═══════════════════════════════[ EXCEPTIONS ]══════════════════════════════╗
-
-# define ERR_MSG_BUF_SIZE 1024
 
 typedef struct s_err_ctx {
 	struct s_err_ctx	*prev_ctx;
 	const char			*file;
-	char				msg[ERR_MSG_BUF_SIZE];
-	int					thrown;
-	int					stage;
-	int					line;
+	jmp_buf				env;
+	char				msg[ERR_MSG_SIZE];
+	int					line:8;
+	int					thrown:8;
 } t_err_ctx;
 
-static ERR_TLS t_err_ctx *g__err_ctx = NULL;
+static ERR_TLS t_err_ctx *g__err = NULL;
 
 
-# define __ERR_INIT ({ \
-	t_err_ctx *prev = g__err_ctx; \
-	g__err_ctx = &__err ; \
-	(t_err_ctx){.prev_ctx=prev, .file=NULL, .msg={0}, \
-			.thrown=0, .stage=0, .line=0}; \
-})
+// ╔═════════════════════════════════[ MACROS ]════════════════════════════════╗
 
-# define __ERR_SET(m, ...) do { \
-	if (!g__err_ctx) break; \
-	g__err_ctx->thrown = 1; \
-	g__err_ctx->line = __LINE__; \
-	g__err_ctx->file = __FILE__; \
-	snprintf(g__err_ctx->msg, ERR_MSG_BUF_SIZE, "Line %d, in %s: "m, __LINE__, __FILE__, ##__VA_ARGS__); \
-} while(0)
 
 # define TRY \
-    for (t_err_ctx __err=__ERR_INIT; __err.stage < 1; g__err_ctx=__err.prev_ctx) \
-		for (; __err.stage < 2; ++__err.stage) \
-			if (__builtin_expect(!__err.stage, 1)) \
-				do
-# define CATCH \
-				while (0); \
-			else if (__builtin_expect(__err.thrown, 0))
+    for (t_err_ctx __err=__ERR_INIT, *__p=&__err; __p; __p=0,g__err=__err.prev_ctx) \
+		if (setjmp(__err.env) == 0) do
 
-# define THROW_IF(cond, msg, ...) {\
-	if (__builtin_expect(!(cond), 0)) {\
-		__ERR_SET(msg, ##__VA_ARGS__); \
-		break; \
+# define CATCH \
+	while (0); \
+	else for (;!__err.thrown; __err.thrown=1)
+
+# define THROW(msg, ...) do { \
+	if (__builtin_expect(g__err != NULL, 1)) { \
+		__ERR_SET(msg, ##__VA_ARGS__) \
+		longjmp(g__err->env, 1); \
 	} \
+} while (0)
+
+# define THROW_IF(cond, msg, ...) do {\
+	if (__builtin_expect((cond), 0)) \
+		THROW(msg, ##__VA_ARGS__); \
+} while (0)
+
+# define ERR_WHY() (g__err ? g__err->msg : "")
+# define ERR_FILE() (g__err ? g__err->file : "")
+# define ERR_LINE() (g__err ? g__err->line : 0)
+
+// ╔══════════════════════════════════[ UTILS ]════════════════════════════════╗
+
+# define __ERR_INIT ({ \
+	t_err_ctx *prev = g__err; \
+	g__err = &__err ; \
+	(t_err_ctx){.prev_ctx=prev, .file=0, .env={0}, .msg={0}, .line=0}; \
+})
+
+# define __ERR_SET(m, ...) \
+	g__err->file = __FILE__; \
+	g__err->line = __LINE__; \
+	snprintf(g__err->msg, ERR_MSG_SIZE, "Line %d, in %s: "m, \
+		__LINE__, __FILE__, ##__VA_ARGS__);
+
+#ifdef __cplusplus
 }
+#endif
--- utests/unittest.c
+++ utests/unittest.c
@@ -1,28 +1,133 @@
+#include "libcerr-log.h"
 #include <utest.h>
 #include <libcerr.h>
 
+# define BAD_TRY_MSG "Reached end of try, FAILURE"
+# define BAD_CATCH_MSG "CATCHED A GHOST, FAILURE"
+# define GOOD_TRY_MSG "Reached end of try, SUCCESS"
+# define GOOD_CATCH_MSG "CATCHED, SUCCESS"
+
+
+static inline void throw(void){
+	THROW(GOOD_CATCH_MSG);
+}
+static inline void divide(int a, int b){
+	THROW_IF(!b, "Division by zero");
+}
 
 __attribute__((constructor))
 void init() { LOG_INFO("LAUNCHING TESTS\n");}
 
-UTEST(libcerr, try) {
-	UTEST_SKIP("Need to implement this test!");
+UTEST(try, simple) {
+	TRY {
+		LOG_DEBUG("Accessing try");
+		LOG_INFO("NOT THROWING...");
+		LOG_OK(GOOD_TRY_MSG);
+	} CATCH {
+		LOG_DEBUG("Accessing catch");
+		LOG_ERR(BAD_CATCH_MSG);
+		ASSERT_TRUE_MSG(0, BAD_CATCH_MSG);
+	}
+	LOG_NL();
 }
 
-UTEST(libcerr, catch) {
-	UTEST_SKIP("Need to implement this test!");
+UTEST(catch, simple) {
+	TRY {
+		LOG_DEBUG("Accessing try");
+		LOG_INFO("THROWING...");
+		THROW(GOOD_CATCH_MSG);
+		LOG_ERR(BAD_TRY_MSG);
+		ASSERT_TRUE_MSG(0, BAD_TRY_MSG);
+	} CATCH {
+		LOG_DEBUG("Accessing catch");
+		LOG_OK("%s", ERR_WHY());
+	}
+	LOG_NL();
 }
 
-UTEST(libcerr, assert) {
-	UTEST_SKIP("Need to implement this test!");
+UTEST(catch, function_call) {
+	TRY {
+		LOG_DEBUG("Accessing try");
+		LOG_INFO("THROWING IN FUNCTION CALL...");
+		throw();
+		LOG_ERR(BAD_TRY_MSG);
+		ASSERT_TRUE_MSG(0, BAD_TRY_MSG);
+	} CATCH {
+		LOG_DEBUG("Accessing catch");
+		LOG_OK("%s", ERR_WHY());
+	}
+	LOG_NL();
 }
 
-UTEST(libcerr, throw) {
-	UTEST_SKIP("Need to implement this test!");
+UTEST(catch, single_nested) {
+	TRY {
+		LOG_DEBUG("Accessing try");
+		TRY {
+			LOG_DEBUG("Accessing nested try");
+			LOG_INFO("THROWING IN NESTED TRY...");
+			THROW(GOOD_CATCH_MSG);
+			LOG_ERR(BAD_TRY_MSG);
+			ASSERT_TRUE_MSG(0, BAD_TRY_MSG);
+		} CATCH {
+			LOG_DEBUG("Accessing nested catch");
+			LOG_OK("nested: %s", ERR_WHY());
+		}
+		LOG_INFO("NOT THROWING...");
+		LOG_OK(GOOD_TRY_MSG);
+	} CATCH {
+		LOG_DEBUG("Accessing catch");
+		LOG_OK("%s", BAD_CATCH_MSG);
+		ASSERT_TRUE_MSG(0, BAD_CATCH_MSG);
+	}
+	LOG_NL();
 }
 
-UTEST(libcerr, throw_if) {
-	UTEST_SKIP("Need to implement this test!");
+UTEST(catch, double_nested) {
+	TRY {
+		LOG_DEBUG("Accessing try");
+		TRY {
+			LOG_DEBUG("Accessing nested try");
+			LOG_INFO("THROWING IN NESTED TRY...");
+			THROW(GOOD_CATCH_MSG);
+			LOG_ERR(BAD_TRY_MSG);
+			ASSERT_TRUE_MSG(0, BAD_TRY_MSG);
+		} CATCH {
+			LOG_DEBUG("Accessing nested catch");
+			LOG_OK("nested: %s", ERR_WHY());
+		}
+		LOG_INFO("THROWING...");
+		THROW(GOOD_CATCH_MSG);
+		LOG_ERR(BAD_TRY_MSG);
+		ASSERT_TRUE_MSG(0, BAD_TRY_MSG);
+	} CATCH {
+		LOG_DEBUG("Accessing catch");
+		LOG_OK("%s", ERR_WHY());
+	}
+	LOG_NL();
+}
+
+UTEST(catch, throw_if) {
+	TRY {
+		LOG_DEBUG("Accessing try");
+		LOG_INFO("THROWING IF, DIVISION BY 0...");
+		divide(1, 0);
+		LOG_ERR(BAD_TRY_MSG);
+		ASSERT_TRUE_MSG(0, BAD_TRY_MSG);
+	} CATCH {
+		LOG_DEBUG("Accessing catch");
+		LOG_OK("%s", ERR_WHY());
+	}
+	TRY {
+		LOG_DEBUG("Accessing try");
+		LOG_INFO("THROWING IF, REGULAR DIVISION...");
+		divide(1, 2);
+		LOG_OK(GOOD_TRY_MSG);
+	} CATCH {
+		LOG_DEBUG("Accessing catch");
+		LOG_OK(BAD_CATCH_MSG);
+		ASSERT_TRUE_MSG(0, BAD_CATCH_MSG);
+	}
+	LOG_NL();
 }
 
 UTEST_MAIN();
