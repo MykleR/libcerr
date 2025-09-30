f09d701e92cfe0994866e3a90d5b8940f43940af
MykleR <michael.rouves@gmail.com>
2025-09-30 18:42:09 +0200

    ✅ fix: Patch utests makefile, moved execution time in tests_main.c

--- utests/tests_main.c
+++ utests/tests_main.c
@@ -1,6 +1,20 @@
+#include "libcerr-log.h"
 #include "tests.h"
+#include <time.h>
+
+static clock_t g_begin;
 
 __attribute__((constructor))
-void init() { LOG_INFO("LAUNCHING TESTS\n");}
+void init() {
+	LOG_INFO("LAUNCHING TESTS\n");
+	g_begin = clock();
+}
+
+__attribute__((destructor))
+void end() {
+	double time_spent = (double)(clock() - g_begin) / CLOCKS_PER_SEC;
+	LOG_NL();
+	LOG_INFO("TESTS ENDED AFTER %lf seconds\n", time_spent);
+}
 
 UTEST_MAIN();
